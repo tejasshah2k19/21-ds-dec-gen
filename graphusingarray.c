e6f37400dc6dad6f9460b1364e15809e1dfe32a8
Tejas <tejas14all@gmail.com>
2022-01-30 10:45:13 +0530

    graph using array

---
+++ graphusingarray.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#define S  4
+
+int graph[S][S];
+
+int main(){
+
+    int i,j,choice;
+
+    //A => 65
+    for(i=0;i<S;i++){
+        for(j=i;j<S;j++){
+            printf("\n%c is connected with %c ",65+i,65+j);//65,65 => A,A
+            printf("\n1 For Yes\n0 For No");
+            scanf("%d",&choice);
+
+            graph[i][j] = choice;
+            graph[j][i] = choice;
+        }
+
+    }
+
+    printf("\nList of Adj --> connected node \n");
+
+   for(i=0;i<S;i++){
+        printf("%c ",65+i);//A
+        for(j=0;j<S;j++){
+            if(graph[i][j]!=0)
+                    printf("%c ",65+j);
+         }
+        printf("\n");
+    }
+
+
+
+    return 0;
+
+}
